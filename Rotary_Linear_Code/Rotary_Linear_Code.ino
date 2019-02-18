da2e2d965e7d28fd9a71b818019250692bef65b8
ovpowar <omkarv.powar@gmail.com>
2019-02-17 19:41:30 -0500

    Create Rotary_Linear_Code.ino
    Rotary Linear Test Code Added
    - Virtual Limit Switching
    - 0 to stop
    - 1 to move in
    - 2 to move out

---
+++ Rotary_Linear_Code/Rotary_Linear_Code.ino
@@ -0,0 +1,63 @@
+#define motorCW 6
+#define motorCCW 8
+#define rotaryEncoderPin A0
+
+float angle_dc;
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(motorCW,OUTPUT);
+  pinMode(motorCCW,OUTPUT);
+   Serial.begin (9600);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+angle_dc = map(analogRead(rotaryEncoderPin),0,1024,0,25);
+
+ Serial.print(angle_dc);
+  Serial.println(",");
+
+  if (Serial.available()>0)
+  {
+    char x = Serial.read();
+  
+  
+    if (x == '0')
+    {
+      digitalWrite(motorCCW,0);
+      digitalWrite(motorCW,0);
+      angle_dc = map(analogRead(rotaryEncoderPin),0,1024,0,25);
+
+    }
+    else if (x == '1')
+    {
+      while (angle_dc > 1)
+      {
+        digitalWrite(motorCCW,0);
+        digitalWrite(motorCW,HIGH);
+        angle_dc = map(analogRead(rotaryEncoderPin),0,1024,0,25);
+        Serial.println(angle_dc);
+      }
+      digitalWrite(motorCCW,0);
+      digitalWrite(motorCW,0);
+    
+    }
+    else if (x == '2')
+    {
+      while(angle_dc < 24)
+      {
+        digitalWrite(motorCCW,HIGH);
+        digitalWrite(motorCW,0);
+        angle_dc = map(analogRead(rotaryEncoderPin),0,1024,0,25);
+        Serial.println(angle_dc);
+      }
+      digitalWrite(motorCCW,0);
+      digitalWrite(motorCW,0);
+    
+    }
+  
+   }
+
+}
